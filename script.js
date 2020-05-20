9c3612e52b531b832a5fa389a203694686c163c0
Dmitry <serbin1337@gmail.com>
2020-05-20 19:44:54 +0300

    commit3
    JSfile has modified one more time

--- script.js
+++ script.js
@@ -9,12 +9,35 @@ const personalDataBase = {
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-      b = prompt('На сколько его оцените?',''),
-      c = prompt('Один из просмотренных фильмов?',''),
-      d = prompt('На сколько его оцените?','');
 
-personalDataBase.movies[a] = b;
-personalDataBase.movies[c] = d;
 
-console.log(personalDataBase);
+for (let i = 0; i <2; i++) {
+    const a = prompt('Один из просмотренных фильмов?', ''),
+          b = prompt('На сколько его оцените?','');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalDataBase.movies[a] = b;
+        console.log('done');
+     
+    } else {
+        console.log('error');
+        i--;
+
+    }      
+
+    
+}
+
+
+if (personalDataBase.count < 10) {
+    console.log("not enough");
+} else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
+    console.log("you are normal viewer");
+}else if (personalDataBase.count >= 30) {
+    console.log('you are typical viewer');
+} else {
+    console.log("Error!");
+}
+
+ console.log(personalDataBase);
+
