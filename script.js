1a962a151beb96f4befe281485a9384ecae4cebd
Dmitry <serbin1337@gmail.com>
2020-05-20 17:59:55 +0300

    JSapp
    JS file has modified

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalDataBase = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
 
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько его оцените?',''),
+      c = prompt('Один из просмотренных фильмов?',''),
+      d = prompt('На сколько его оцените?','');
 
+personalDataBase.movies[a] = b;
+personalDataBase.movies[c] = d;
 
-const user = "Ivan";
-
-alert(`привет, ${user}`);
+console.log(personalDataBase);
