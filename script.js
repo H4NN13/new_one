6fb484a92280fc86ab5a543d72c2e1607997285b
Dmitry <serbin1337@gmail.com>
2020-05-21 18:08:44 +0300

    commited function practice
    практические задания с функциями

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    }
+}
+
+start();
 
 const personalDataBase = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
 
 
-for (let i = 0; i <2; i++) {
-    const a = prompt('Один из просмотренных фильмов?', ''),
-          b = prompt('На сколько его оцените?','');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalDataBase.movies[a] = b;
-        console.log('done');
-     
-    } else {
-        console.log('error');
-        i--;
 
-    }      
-
+function rememberMyFilms() {
+    for (let i = 0; i <2; i++) {
+        const a = prompt('Один из просмотренных фильмов?', ''),
+              b = prompt('На сколько его оцените?','');
     
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalDataBase.movies[a] = b;
+            console.log('done');
+         
+        } else {
+            console.log('error');
+            i--;
+    
+        }      
+    
+        
+    }
+
+}
+
+//rememberMyFilms();
+
+
+
+function detectPersonalLevel () {
+    if (personalDataBase.count < 10) {
+        console.log("not enough");
+    } else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
+        console.log("you are normal viewer");
+    }else if (personalDataBase.count >= 30) {
+        console.log('you are typical viewer');
+    } else {
+        console.log("Error!");
+    }
+}
+//detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalDataBase);
+    }
 }
 
 
-if (personalDataBase.count < 10) {
-    console.log("not enough");
-} else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
-    console.log("you are normal viewer");
-}else if (personalDataBase.count >= 30) {
-    console.log('you are typical viewer');
-} else {
-    console.log("Error!");
-}
+ showMyDB(personalDataBase.privat);
 
- console.log(personalDataBase);
+ function writeYourGenres() {
+     for (let i = 1; i <= 3; i++) {
+         personalDataBase.genres[i - 1] = prompt(`ваш любимый жанр под номером ${i}`);
+     }
+ }
+ writeYourGenres();
 
