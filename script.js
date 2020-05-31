e1c9dd0ad7f59671683c1ce7d01a4bb93bf89d08
Dmitry <serbin1337@gmail.com>
2020-05-31 17:47:45 +0300

    zaebalsya
    kill me pls

--- script.js
+++ script.js
@@ -1,79 +1,102 @@
 
-let numberOfFilms;
-
-function start () {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    }
-}
-
-start();
 
 const personalDataBase = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalDataBase.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalDataBase.count == '' || personalDataBase.count == null || isNaN(personalDataBase.count)) {
+            personalDataBase.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i <2; i++) {
+            const a = prompt('Один из просмотренных фильмов?', ''),
+                  b = prompt('На сколько его оцените?','');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalDataBase.movies[a] = b;
+                console.log('done');
+             
+            } else {
+                console.log('error');
+                i--;
+        
+            }      
+        
+            
+        }
+    
+    },
+    detectPersonalLevel: function () {
+        if (personalDataBase.count < 10) {
+            console.log("not enough");
+        } else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
+            console.log("you are normal viewer");
+        }else if (personalDataBase.count >= 30) {
+            console.log('you are typical viewer');
+        } else {
+            console.log("Error!");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalDataBase);
+        }
+    },
+    toggleVisMyDB: function() {
+        if (personalDataBase.privat) {
+            personalDataBase.privat = false;
+        } else {
+            personalDataBase.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`ваш любимый жанр под номером ${i}`)
+
+            if (genre === ''|| genre == null) {
+                console.log('вы ввели некорректные данные или их нет:(');
+                i--;
+            } else {
+                personalDataBase.genres[i - 1] = genre;    
+        }
+    }    
+            
+    personalDataBase.genres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i +1} - это ${item}`);
+    });    
+    }
 };
 
 
 
 
 
-function rememberMyFilms() {
-    for (let i = 0; i <2; i++) {
-        const a = prompt('Один из просмотренных фильмов?', ''),
-              b = prompt('На сколько его оцените?','');
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalDataBase.movies[a] = b;
-            console.log('done');
-         
-        } else {
-            console.log('error');
-            i--;
-    
-        }      
-    
-        
-    }
-
-}
-
-//rememberMyFilms();
 
 
 
-function detectPersonalLevel () {
-    if (personalDataBase.count < 10) {
-        console.log("not enough");
-    } else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
-        console.log("you are normal viewer");
-    }else if (personalDataBase.count >= 30) {
-        console.log('you are typical viewer');
-    } else {
-        console.log("Error!");
-    }
-}
-//detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalDataBase);
-    }
-}
 
 
- showMyDB(personalDataBase.privat);
 
- function writeYourGenres() {
-     for (let i = 1; i <= 3; i++) {
-         personalDataBase.genres[i - 1] = prompt(`ваш любимый жанр под номером ${i}`);
-     }
- }
- writeYourGenres();
+
 
